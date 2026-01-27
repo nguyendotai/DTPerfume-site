ccc7acbefc3af6df2e8a2ef9d3d32be3f2c83fa2
nguyendotai <nguyendotai11@gmail.com>
2026-01-27 20:52:09 +0700

    pushs again

--- app/lib/mock.ts
+++
@@ -1,9 +0,0 @@
-import { readMockServer } from "./mock.server";
-import { readMockClient } from "./mock.client";
-
-export async function readMock<T>(fileName: string): Promise<T> {
-  if (typeof window === "undefined") {
-    return readMockServer<T>(fileName);
-  }
-  return readMockClient<T>(fileName);
-}
